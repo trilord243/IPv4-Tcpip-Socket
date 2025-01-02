5e4390a8141f303ab016e0344122699e7db2e121
Felipe Escalona <escalonaf12@gmail.com>
2025-01-02 14:10:20 -0400

    get ip address using hostname

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #TCP server side
 import socket
-#Create a server side socket IPV4 (AF_INET) adn TCP (Sock_sTREAM)
-server_socket =
+#Create a server side socket IPV4 (AF_INET) adn TCP (SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#See how to get IP address dynamically
+
+print(socket.gethostname())#HostName
+print(socket.gethostbyname(socket.gethostname()))#Get ip address using the Hostname
